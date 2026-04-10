31b7245ecb6f926d43e7d37f95e343da96e46521
Threemasks112 <jongm@uia.no>
2026-04-10 12:10:14 +0200

    Cars an physics
    Added coliders to the cars so now they move on impact by the hamster

--- Assets/Scripts/LaunchCT(OBJ).cs
+++ Assets/Scripts/LaunchCT(OBJ).cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
-public class LaunchCT(OBJ) : MonoBehaviour
+public class LaunchCT : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public float launchForce = 8f;
+
+    void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.collider.CompareTag("OBJ"))
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            Vector3 direction = (transform.position - collision.transform.position).normalized;
+            rb.AddForce(direction * launchForce, ForceMode.Impulse);
+        }
     }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
 }
