2d76ee543ff49ba694ac4d76c1a142d624ad44c1
Threemasks112 <jongm@uia.no>
2026-04-10 15:26:42 +0200

    more movement
    Added mroe movement to teh hamster as he looks around and bump into objects

--- Assets/Scripts/Start Button E.cs
+++ Assets/Scripts/Start Button E.cs
@@ -6,8 +6,19 @@ public class StartButtonE : MonoBehaviour
     public AudioSource music;
     public float rollforce = 10f;
 
+    public Transform[] waypoints;
+    public float moveSpeed = 5f;
+    public float reachDistance = 1f;
+
     private Rigidbody rb;
     private bool started = false;
+    private int hitCounter = 0;
+    private bool followPath = false;
+    private int currentWaypoint = 0;
+
+
+
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,5 +38,49 @@ public class StartButtonE : MonoBehaviour
 
             rb.AddForce(transform.forward * rollforce, ForceMode.Impulse);
         }
+         if (followPath)
+        {
+            FollowWaypoints();
+        }
     }
+
+    [System.Obsolete]
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("OBJ"))
+        {
+            hitCounter++;
+
+           if (hitCounter == 1)
+            {
+                // First hit → normal bounce
+                Vector3 direction = (transform.position - collision.transform.position).normalized;
+                rb.AddForce(direction * rollforce, ForceMode.Impulse);
+            }
+            else if (hitCounter == 3)
+            {
+               followPath = true;
+                rb.velocity = Vector3.zero; // stop rolling
+                rb.isKinematic = true;
+            }
+        }
+    }
+
+    void FollowWaypoints()
+    {
+        if (currentWaypoint >= waypoints.Length)
+            return;
+
+        Transform target = waypoints[currentWaypoint];
+
+        // Move toward waypoint
+        transform.position = Vector3.MoveTowards(transform.position,target.position,moveSpeed * Time.deltaTime);
+
+        // Check if reached
+        if (Vector3.Distance(transform.position, target.position) < reachDistance)
+        {
+            currentWaypoint++;
+        }
+    }
+    
 }
